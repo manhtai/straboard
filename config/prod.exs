120166977f8942053b9ec33ffb0c77e323a27f54
manhtai <vo@manhtai.com>
2021-02-28 13:23:38 +0700

    Config for deploy

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :straboard, StraboardWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "straboard.herokuapp.com", port: 443],
+  http: [port: {:system, "PORT"}],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
